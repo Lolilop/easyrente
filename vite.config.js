00b1ba47a11caf110b0fa6933d95641f35d224cc
Your Name <you@example.com>
2025-06-04 20:21:24 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/easyrente/", // ← CHANGE ceci avec le nom EXACT de ton repo GitHub
+  base: "/easyrente/",
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
